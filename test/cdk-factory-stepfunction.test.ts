5c094394f0fdbc6fa47c5f3470195a3ed82bb385
seitamuro <seitamuro0210@gmail.com>
2024-10-16 22:29:31 +0900

    Initial commit

---
+++ test/cdk-factory-stepfunction.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkFactoryStepfunction from '../lib/cdk-factory-stepfunction-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-factory-stepfunction-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkFactoryStepfunction.CdkFactoryStepfunctionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
